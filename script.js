ee520c0e2e8bf6de439526d0c034d12ad358fe3a
FeketeNemetZsofia <zsofia.nemet91@gmail.com>
2023-10-12 09:51:33 +0200

    trying to change the code

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-function stringSplosion(str){
-let newStr = ""
-for (let i = 0; i < str.length; i++){
-    for (let c = 0; c <= i; c++){ //Addig számol amíg el nem ér az indexig, s ismétli a folyamatot.
-        newStr += str[c]
-    }
-}
-console.log(newStr)
-  }
+let arr = ["kutya", "cica", "teki"]
 
-stringSplosion("Code")
+arr.forEach(function(element))
+{ console.log(element)}
